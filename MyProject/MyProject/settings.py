103ef0e63524b1f74827c541dcb6217dd2ef4644
mgh-github <mghthakur1@gmail.com>
2021-04-02 12:14:43 +0530

    deployment changes

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y62f+$ifg39g@ed+flvz3d^p1t@+(!3xac+#vqflvgse71+c9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','useronboarding.herokuapp.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
